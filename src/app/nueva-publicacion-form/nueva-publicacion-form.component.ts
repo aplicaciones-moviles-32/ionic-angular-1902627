5d1f94219fdc85d498eacd1acf0edd8a8d0bd5a0
Abraham Ramirez <abrahammoreno044@gmail.com>
2022-04-09 14:28:49 -0500

    Agregado form publicación

--- src/app/nueva-publicacion-form/nueva-publicacion-form.component.ts
+++ src/app/nueva-publicacion-form/nueva-publicacion-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { publicacion } from '../interfaces';
 import { BdatosService } from '../bdatos.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-nueva-publicacion-form',
@@ -14,14 +15,14 @@ export class NuevaPublicacionFormComponent implements OnInit {
   ngOnInit(): void {
   }
   
-  publicacionNueva: publicacion = {
+  pub: publicacion = {
     'caption': "",
     'imagen': "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y",
     'usuario': "test"
   }
 
-  publicar() {
-    this.db.postPublicacion(this.publicacionNueva)
+  onSubmit(datos: NgForm) {
+    this.db.postPublicacion(this.pub)
   }
 
 }
